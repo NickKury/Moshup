1b54d6432682112bc07824203f366fca8539dd29
Nick Kury <nickkury12@gmail.com>
2021-05-24 18:40:03 -0700

    event format

--- frontend/src/components/CreateEventForm/CreateEventForm.js
+++ frontend/src/components/CreateEventForm/CreateEventForm.js
@@ -8,24 +8,6 @@ export function CreateEventButton() {
     const dispatch = useDispatch();
     const sessionUser = useSelector((state) => state.session.user);
     const [errors, setErrors] = useState([]);
-//     // const history = useHistory()
-
-//     // if (sessionUser) {
-//     //   history.push('/')
-//     // };
-  
-//     // const handleSubmit = (e) => {
-//     //   e.preventDefault();
-//     //   if (true) {
-//     //     setErrors([]);
-//     //     return dispatch(sessionActions.create({ description, userId, genreId,date }))
-//     //       .catch(async (res) => {
-//     //         const data = await res.json();
-//     //         if (data && data.errors) setErrors(data.errors);
-//     //       });
-//     //   }
-//     //   return setErrors(['Confirm Password field must be the same as the Password field']);
-//     // };
 
     return(
         <div>
@@ -35,23 +17,3 @@ export function CreateEventButton() {
         </div>
     )
 }
-// //  function CreateEventForm() {
-// //     const dispatch = useDispatch();
-// //     const sessionUser = useSelector((state) => state.session.user);
-// //     const [email, setEmail] = useState("");
-// //     const [username, setUsername] = useState("");
-// //     const [password, setPassword] = useState("");
-// //     const [confirmPassword, setConfirmPassword] = useState("");
-// //     const [errors, setErrors] = useState([]);
-// //     const history = useHistory()
-
-// //     return(
-// //         <div>Eventpage
-// //          <button id='create-event-button'>
-// //             <Link exact to="/new-event">Create an Event</Link>
-// //         </button>
-// //         </div>
-// //     )
-// // }
-
-// export default CreateEventButton;
--- frontend/src/components/CreateEventForm/index.js
+++ frontend/src/components/CreateEventForm/index.js
@@ -5,50 +5,13 @@ import CreateEvent from './CreateEvent'
 // // import {create} from ''
 
 const CreateEventPage = () => {
-    const dispatch = useDispatch();
-    const genres = useSelector(state => Object.values(state.genres)); //obj.values changes from an array to an obj
-    // console.log(genres, 'stuff')
+   
     return(
         <div>
             <CreateEvent/>
         </div>
     )
 }
-    // use react hook and cause a side effect
-//     useEffect(() => {
-//         dispatch(getGenres());
-//     }, [dispatch]);
-//     return(
-//         <div className='containers-div'>
-//            im a div
-//            <div>
-//                <textarea>
-//                    Who is Playing?
-//                </textarea>
-//            </div>
-//            <div>
-//                 when is your event
-//                 <input type='date'>
-
-//                 </input>
-//            </div>
-//            <div>
-//                what genre is your event
-//                 <form  method='post' >
-//                     <select>
-//                         {genres.map(genre => 
-//                         <option >
-//                             {genre.name} 
-//                         </option>
-//                          )}  
-//                     </select>
-//                 </form>
-//             </div>
-//             <button type='submit'>
-//                 Create your event
-//             </button>
-//         </div>
-//     )
-// }
+  
 
 export default CreateEventPage;
